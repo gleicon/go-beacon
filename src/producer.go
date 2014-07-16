ad9bd72ed0dd86b64e6a6dd5ff181bf0044b8346
Gleicon Moraes <gleicon@gmail.com>
2014-07-16 19:00:46 -0300

    irresponsible method

--- src/producer.go
+++ src/producer.go
@@ -11,12 +11,20 @@ var (
 )
 
 func send(query url.Values) error {
-	var b []byte
-	enc := codec.NewEncoderBytes(&b, &mh)
-	err := enc.Encode(query)
+	err, b := encode(query)
 	if err != nil {
 		return err
 	}
 	fmt.Println(string(b))
 	return nil
+}
+
+func encode(query url.Values) (error, []byte) {
+	var b []byte
+	enc := codec.NewEncoderBytes(&b, &mh)
+	err := enc.Encode(query)
+	if err != nil {
+		return err, nil
+	}
+	return nil, b
 }
