c375a05a03fc0657692585965fe53a1e6c4003b1
Gleicon Moraes <gleicon@gmail.com>
2014-07-17 11:40:50 -0300

    added mangos + producer w/ a local pushback queue

--- src/producer.go
+++ src/producer.go
@@ -1,21 +1,86 @@
 package main
 
 import (
-	"fmt"
+	"bitbucket.org/gdamore/mangos/protocol/req"
+	"bitbucket.org/gdamore/mangos/transport/all"
+	"container/list"
+	"errors"
 	"github.com/ugorji/go/codec"
 	"net/url"
+	"time"
 )
 
+// TODO: fetch url from configuration file
+// TODO: implement buffer size counter
+// TODO: push flush time to config
+// TODO: move it to struct
+// TODO: look into creating a conn pool
+
 var (
-	mh codec.MsgpackHandle
+	mh            codec.MsgpackHandle
+	buffer        *list.List
+	backendUrl    string
+	flushInterval int
 )
+
+func initProducer(u string, flushInt int) {
+	buffer = list.New()
+	backendUrl = u
+	flushInterval = flushInt
+	go func() {
+		time.Sleep(time.Duration(flushInterval) * time.Second)
+		flushBuffer()
+	}()
+}
+
+func flushBuffer() {
+	for i := buffer.Front(); i != nil; i = i.Next() {
+		wat := i.Value.([]byte)
+		err := sendMessage(&wat)
+		if err != nil {
+			// TODO: retry counter
+			buffer.PushBack(wat)
+		}
+	}
+}
+
+func sendMessage(message *[]byte) error {
+	requestSocket, err := req.NewSocket()
+	if err != nil {
+		return err
+	}
+	defer requestSocket.Close()
+	all.AddTransports(requestSocket)
+	if err = requestSocket.Dial(backendUrl); err != nil {
+		return err
+	}
+
+	if err = requestSocket.Send(*message); err != nil {
+		return err
+	}
+
+	var clientMsg []byte
+
+	if clientMsg, err = requestSocket.Recv(); err != nil {
+		return err
+	}
+
+	if string(clientMsg) != "OK" {
+		return errors.New("Response not OK, requeued")
+	}
+
+	return nil
+}
 
 func send(query url.Values) error {
 	err, b := encode(query)
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(b))
+	err = sendMessage(&b)
+	if err != nil {
+		buffer.PushBack(b)
+	}
 	return nil
 }
 
